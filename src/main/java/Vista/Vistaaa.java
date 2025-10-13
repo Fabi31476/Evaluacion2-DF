6d8ba2a221e48272791f55438a788681836646db
Fabi31476 <f.morales103@alumnos.santotomas.cl>
2025-10-13 02:27:48 -0300

    se corrigen errores.

--- src/main/java/Vista/Vistaaa.java
+++ src/main/java/Vista/Vistaaa.java
@@ -21,7 +21,7 @@ import javax.swing.UnsupportedLookAndFeelException;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 import javax.swing.table.DefaultTableModel;
 
-public class Vista extends JFrame {
+public class Vistaaa extends JFrame {
 
     private JTextField txtCodigo, txtNombre, txtContinente, txtPoblacion, txtBuscar, txtFiltro;
     private JButton btnAgregar, btnEliminar, btnBuscar, btnFiltrar, btnTop;
@@ -30,7 +30,7 @@ public class Vista extends JFrame {
     private final Controlador controlador;
     private int codigoSeleccionado = -1;
 
-    public Vista() {
+    public Vistaaa() {
         controlador = new Controlador();
         setTitle("Gestión de Países");
         setSize(650, 400);
@@ -197,25 +197,19 @@ public class Vista extends JFrame {
                 }
             }
         } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException ex) {
-            Logger.getLogger(Vista.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Vistaaa.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         
     }
-}
+
 
 /**
  *
  * @author fchmm
  */
-public class Vistaaa extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Vistaaa
-     */
-    public Vistaaa() {
-        initComponents();
-    }
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -245,38 +239,13 @@ public class Vistaaa extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Vistaaa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Vistaaa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Vistaaa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Vistaaa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    }   
+           
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Vistaaa().setVisible(true);
-            }
-        });
-    }
+     
+
+        
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
-}
+
