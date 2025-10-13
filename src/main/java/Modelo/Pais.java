edc7b7cfdb643af5a07f495b3658e9647757bd5d
Fabi31476 <fchmm31476@gmail.com>
2025-10-12 22:59:07 -0300

    Siguiendo los pasos..

---
+++ src/main/java/Modelo/Pais.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+public class Pais {
+
+    protected int codigo;
+    protected String nombrePais;
+    protected String continente;
+    protected int poblacion;
+
+    public Pais() {
+    }
+
+    public Pais(int codigo, String nombrePais, String continente, int poblacion) {
+        this.codigo = codigo;
+        this.nombrePais = nombrePais;
+        this.continente = continente;
+        this.poblacion = poblacion;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombrePais() {
+        return nombrePais;
+    }
+
+    public void setNombrePais(String nombrePais) {
+        this.nombrePais = nombrePais;
+    }
+
+    public String getContinente() {
+        return continente;
+    }
+
+    public void setContinente(String continente) {
+        this.continente = continente;
+    }
+
+    public int getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(int poblacion) {
+        this.poblacion = poblacion;
+    }
+
+}
---
+++ src/main/java/com/mycompany/dfproyecto/DfProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dfproyecto;
+
+/**
+ *
+ * @author fchmm
+ */
+public class DfProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
