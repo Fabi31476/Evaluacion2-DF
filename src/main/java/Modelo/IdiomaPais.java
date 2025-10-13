06580bd8854626fb593b2fe850c2e559e4063f5f
Fabi31476 <f.morales103@alumnos.santotomas.cl>
2025-10-12 23:32:25 -0300

    Agregue Atributos, getters y setters de idiomaPais.

---
+++ src/main/java/Modelo/IdiomaPais.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author fchmm
+ */
+public class IdiomaPais {
+    
+    private String idioma;
+    private boolean oficial;
+    private double porcentaje;
+    // Getters y setters
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public boolean isOficial() {
+        return oficial;
+    }
+
+    public void setOficial(boolean oficial) {
+        this.oficial = oficial;
+    }
+
+    public double getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(double porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+   
+    
+}
